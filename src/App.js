115ff16f7d341a1b1a2caf962726a4fc00a6839c
shamaz332 <shamazsaeed332@gmail.com>
2020-07-07 17:52:01 +0500

    Reading URL Parameters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Home} from "./components/home.js"
 import {About} from "./components/about.js"
+import {Product} from "./components/product"
 import {Error} from "./components/404.js"
-import {Routes,Route} from 'react-router'
+import {Routes,Route} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 function App() {
@@ -10,14 +11,15 @@ function App() {
     <div className="App">
       <h1>Hello App</h1>
 <Link to="/" >Home</Link>
-<br/>
+{" "}
 <Link to="/about" >About</Link>
-
-
+{" "}
+<Link to="/product" >Product</Link>
 <Routes>
 
 <Route path="/" element={<Home/>}/>
 <Route path="/about" element={<About/>}/>
+<Route path="/product/:productID" element={<Product/>}/>
 <Route path="*" element={<Error/>}/>
 </Routes>
 
