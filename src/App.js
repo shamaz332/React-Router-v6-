941086a9ee4c1073331cfe1ac34de66845e63f53
shamaz332 <shamazsaeed332@gmail.com>
2020-07-07 17:24:45 +0500

    route working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './App.css';
+import {Home} from "./components/home.js"
+import {About} from "./components/about.js"
+import {Routes,Route} from 'react-router'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello boilerplate</h1>
+      <h1>Hello App</h1>
+<Link to="/" >Home</Link>
+<br/>
+<Link to="/about" >About</Link>
+
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+<Route path="/about" element={<About/>}/>
+</Routes>
+
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>My about</h1>
+        </div>
+    )
+}
